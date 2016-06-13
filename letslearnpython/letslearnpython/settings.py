dc293b6fb75a947216873b934c1743c778a44e0d
mechanicalgirl <barbara.shaurette@gmail.com>
2016-06-13 13:33:05 -0500

    fix fixtures and settings

--- letslearnpython/letslearnpython/settings.py
+++ letslearnpython/letslearnpython/settings.py
@@ -84,7 +84,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-
-FIXTURE_DIRS = (
-   os.path.join(BASE_DIR, '/fixtures/')
-)
