348e759c511f69bd5a95b20b5dc4cc2bf172ff66
Andrew Dupont <github@andrewdupont.net>
2016-07-04 14:15:48 -0500

    Independence Day work:
    * Moved JavaScript to its own file.
    * Added ability to toggle answer blocks, which are collapsed by default.
    * Fixed markup on tables.
    * Made all images responsive.

---
+++ letslearnpython/lessons/static/lessons/js/index.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function () {
+  
+
+  // Allow the user to hide/show the menu if they're in a narrow viewport.
+  $('.yk-toggle-menu').click(function (e) {
+    e.preventDefault();
+    var $menu = $('.yk-container-nav');
+    var newText;
+    $menu.toggleClass('yk-menu-collapsed');
+    if ( $menu.hasClass('yk-menu-collapsed') ) {
+      newText = 'Show menu';
+    } else {
+      newText = 'Hide menu';
+    }
+    $('.yk-toggle-menu').text(newText);
+  });
+  
+  
+  // If the "hide/show menu" link is visible, it means we're at a narrow
+  // breakpoint, so the menu should be collapsed by default.
+  if ( $('.yk-toggle-menu:visible').length ) {
+    $('.yk-container-nav').addClass('yk-menu-collapsed');
+  }
+  
+  // All the "answer" sections should toggle and be collapsed by default.
+  $('pre.toggle').each(function (i, pre) {
+    var $pre = $(pre);
+    $pre.addClass('toggle-collapsed');
+    
+    $pre.click(function () {
+      console.log('click!');
+      var shouldBeExpanded = $pre.hasClass('toggle-collapsed');
+      $pre.toggleClass('toggle-collapsed', !shouldBeExpanded);
+    });
+    
+  });
+  
+  
+});
+
+
+
