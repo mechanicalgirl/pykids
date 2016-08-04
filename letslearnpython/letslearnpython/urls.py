3a4a14b34c05cb56dc38306ff50cad2e65bdc008
mechanicalgirl <barbara.shaurette@gmail.com>
2016-08-04 16:48:33 -0500

    add a favicon

--- letslearnpython/letslearnpython/urls.py
+++ letslearnpython/letslearnpython/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 admin.autodiscover()
 
 from . import views
@@ -11,5 +13,6 @@ urlpatterns = [
     url(r'^contact/', views.contactus, name='contact'),
     url(r'^landing/', include('landing_app.urls')),
     url(r'^admin/',   include(admin.site.urls)),
+    url(r'^favicon\.ico$',  RedirectView.as_view(url='/static/letslearnpython/images/favicon.ico')),
     url(r'^$',        views.index, name='index'),
 ]
